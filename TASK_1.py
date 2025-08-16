7db36596253270790af90b5318403818a81a7407
vinitvihol <142862127+vinitvihol@users.noreply.github.com>
2025-08-16 09:24:00 +0530

    Add files via upload

---
+++ TASK_1.py
@@ -0,0 +1,15 @@
+A=input("Enter the First Number: ")
+A=int(A)
+B=input("Enter the Second Number: ")
+B=int(B)
+SUM=A+B
+SUB=A-B
+MUL=A*B
+DIV=A/B
+print("Addition: ",SUM)
+print("Subtraction: ",SUB)
+print("Multiplication: ",MUL)
+print("Division: ",DIV)
+
+
+
